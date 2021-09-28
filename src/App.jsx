27ee6ef4c0403bc6aa9198135ca9f9ed4b131a01
Luahp <luahporto@gmail.com>
2021-09-28 17:07:52 -0300

    all app

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Tasks from './components/Tasks';
+
+import './App.css';
+import AddTask from './components/AddTask';
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Estudar Inglês',
+      completed: true,
+    },
+  ]);
+
+  const handleTaskAddition = taskTitle => {
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: Math.random(10),
+        completed: false,
+      },
+    ];
+    setTasks(newTasks);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} />
+      </div>
+    </>
+  );
+};
+
+export default App;
