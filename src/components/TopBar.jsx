797cc2f6bcdf2f3055c765163c9148cba15dde05
Luahp <luahporto@gmail.com>
2021-09-30 08:24:18 -0300

    topbar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import './TopBar.css';
 
 const TopBar = () => {
-    return (
+  return (
     <div class="topnav">
-        <a href="#home">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+      <a href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
     </div>
-    );
-}
+  );
+};
 
 export default TopBar;
